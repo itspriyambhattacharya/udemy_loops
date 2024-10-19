062d03ba76161bc67281836d4063b05c0b571469
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-19 19:15:35 +0530

    modified question 1 solution

--- 01_loops_challenges.js
+++ 01_loops_challenges.js
@@ -1,10 +1,12 @@
 //Question 1
 let arr1 = ["green tea", "black tea", "chai", "oolong tea"];
+let selectedTeas = [];
 for (let i = 0; i < arr1.length; i++) {
   const element = arr1[i];
   if (element === "chai") {
     break;
   } else {
-    console.log(element);
+    selectedTeas.push(element);
   }
 }
+console.log(selectedTeas);
